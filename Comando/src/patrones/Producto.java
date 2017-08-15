a6cb8ac9fdb52452182c62a17eb7399e46fa86b3
dusgab <dustingassmann@gmail.com>
2017-08-15 15:57:09 -0300

    Patron Comando

---
+++ Comando/src/patrones/Producto.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package patrones;
+
+/**
+ * @author DUSGAB
+ *
+ */
+public class Producto {
+
+	private int id;
+	private String nombre;
+	private double precio;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+}
