f542fd452ffc9b9d37e4e31d64f52b997ec3ae33
dusgab <dustingassmann@gmail.com>
2017-08-15 14:48:05 -0300

    Aplicando Delegacion a la Clase Coche

--- Delegacion/src/patrones/Coche.java
+++ Delegacion/src/patrones/Coche.java
@@ -28,4 +28,24 @@ public class Coche {
 		this.marca = marca;
 	}
 
+	// Metodo agregado mediante la aplicacion de Delegacion
+	public int getPotencia() {
+		return motor.getPotencia();
+	}
+
+	// Metodo agregado mediante la aplicacion de Delegacion
+	public void setPotencia(int potencia) {
+		motor.setPotencia(potencia);
+	}
+
+	// Metodo agregado mediante la aplicacion de Delegacion
+	public int getCilindros() {
+		return motor.getCilindros();
+	}
+
+	// Metodo agregado mediante la aplicacion de Delegacion
+	public void setCilindros(int cilindros) {
+		motor.setCilindros(cilindros);
+	}
+
 }
