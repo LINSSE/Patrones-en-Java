594b1f10cdcc1809bfb3c25c5f21a900235db798
dusgab <dustingassmann@gmail.com>
2017-08-21 18:13:20 -0300

    Patron Fachada

---
+++ Fachada/src/patrones/PrincipalCliente.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package patrones;
+
+/**
+ * @author DUSGAB
+ *
+ */
+public class PrincipalCliente {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Impresora i = new Impresora();
+		i.setHoja("a4");
+		i.setColor(true);
+		i.setTipoDocumento("pdf");
+		i.setTexto("texto 1");
+		i.imprimirDocumento();
+
+		Impresora i2 = new Impresora();
+		i2.setHoja("a4");
+		i2.setColor(true);
+		i2.setTipoDocumento("pdf");
+		i2.setTexto("texto 2");
+		i2.imprimirDocumento();
+
+		Impresora i3 = new Impresora();
+		i3.setHoja("a3");
+		i3.setColor(false);
+		i3.setTipoDocumento("excel");
+		i3.setTexto("texto 3");
+		i3.imprimirDocumento();
+	}
+
+}
